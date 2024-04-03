3e630a7a412c842d9ce9ef9a94101759f637b649
lvjibin <517752548@qq.com>
2024-04-03 12:31:27 +0800

    输出加预测

--- ADBControl.py
+++ ADBControl.py
@@ -2,6 +2,7 @@ import os
 import platform
 import subprocess
 import sys
+from adbutils import adb
 
 adb_path = "D:\\Softs\\Unity\\2022.3.22f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\platform-tools\\adb.exe"
 def run_adb_shell_command(command):
@@ -16,26 +17,27 @@ def run_adb_shell_command(command):
         return None
 
 
-def is_android_device_connected():
+def run_adb_shell_command_byId(deviceid,command):
+    # 构建完整的adb shell命令
+    full_command = [adb_path,"-s",deviceid, command]
+    # 使用subprocess执行命令
+    try:
+        result = subprocess.check_output(full_command, stderr=subprocess.STDOUT, shell=False)
+        return result # 将字节串解码为字符串并去除首尾空白字符
+    except subprocess.CalledProcessError as e:
+        print(f"命令执行失败: {e}")
+        return None
+#
+def Collect_android_device_connected():
     """检查是否有Android设备连接"""
+    devices = []
     result = run_adb_shell_command('devices').decode('utf-8').strip()
-    for i in range(1, len(result) - 1):
-        if result[i].startswith("*") and result[i].endswith("*"):
-            pass
-        else:
-            print(result[i].split("\t"))
+    lines = result.split('\n')
+    for line in lines:
+        if line.endswith('device'):
+            devices.append(line.split()[0])
+    return devices
 
-
-# # 检查是否有设备连接
-# if is_android_device_connected():
-#     # 执行ADB Shell命令
-#     output = run_adb_shell_command('ls /sdcard/')
-#     if output:
-#         print(f"命令输出: {output}")
-#     else:
-#         print("命令执行失败")
-# else:
-#     print("没有设备连接")
 
 def adb_screenshot(local_path):
 
@@ -56,17 +58,28 @@ def adb_screenshot(local_path):
 # adb_screenshot("D:\\temp\\1.png")
 # is_android_device_connected()
 
-def capture_android(device_id):
-    sh1 = "adb -s " + device_id + " shell /system/bin/screencap -p /sdcard/screenshot.png"
-    sh2 = "adb -s " + device_id + " pull /sdcard/screenshot.png d:/screenshot.png"
-    execute_shell(sh1)
-    execute_shell(sh2)
+def capture_android(device):
+    # 执行screencap命令并保存截图到本地文件
+    # 注意：screencap命令可能因Android版本和设备制造商的不同而有所差异
+    screenshot_path_on_device = "/sdcard/screenshot.png"
+    screenshot_path_on_host = "D:\\temp/screenshot.png"
+    # 在设备上执行screencap命令
+    # device.shell(f"screencap -p {screenshot_path_on_device}")
+    # 从设备拉取截图文件到本地
+    image = device.screenshot()
+    return image
+
+
+
 
 
 def execute_shell(shell):
     p = subprocess.Popen(shell, shell=True, stdout=subprocess.PIPE)
     out = p.stdout.readlines()
     return out
+
+def Touch_android(deviceid,x,y):
+    result = run_adb_shell_command_byId(deviceid,'input tap ' + x + " " + y)
 
 
 # if __name__ == '__main__':
@@ -76,4 +89,25 @@ def execute_shell(shell):
 #         capture_android(sys.argv[1])
 #         if ("Windows" in platform.platform()):
 #             os.startfile("d:\\screenshot.png")
-is_android_device_connected()
+
+# devices = Collect_android_device_connected()
+# for device in devices:
+#     Touch_android(device,100,100)
+
+# 确保ADB服务器已经启动，并且设备已经连接
+
+
+def Get_ScreenShot():
+    devices = adb.device_list()
+    if devices:
+        device = devices[0]  # 选择第一个连接的设备
+        print(f"Connected device: {device}")
+        return capture_android(device)
+    else:
+        print("No device connected.")
+        return None
+
+# # 模拟点击屏幕坐标 (x, y)
+# x = 500
+# y = 1000
+# device.shell(f'input tap {x} {y}')
--- run.py
+++ run.py
@@ -1,14 +1,19 @@
+import ADBControl
 from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Load a model
     model = YOLO("yolov8s.yaml")  # build a new model from scratch
-    model = YOLO("weight/yolov8s.pt")  # load a pretrained model (recommended for training)
-    # model = YOLO("runs/detect/train40/weights/best.pt")
+    # model = YOLO("weight/yolov8s.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("runs/detect/train/weights/best.pt")
 
     # source = 'weight/斗地主4.mp4'
     # Use the model
-    model.train(data="datasets/epgame/epgame.yaml", epochs=3)  # train the model
+    # model.train(data="datasets/epgame/epgame.yaml", epochs=3)  # train the model
     # metrics = model.val()  # evaluate model performance on the validation set
     # results = model.predict(source,save=True,vid_stride=100)  # predict on an image
     # path = model.export(format="onnx")  # export the model to ONNX format
+
+    #通过截图安卓屏幕来预测
+    # image = ADBControl.Get_ScreenShot()
+    # results = model.predict(image,save=True,vid_stride=100,conf=0.7,max_det=20)
